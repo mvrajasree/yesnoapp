c7b67f28b2b90a356f1188448760d462a8ba8985
mvrajasree <rajasree050405@gmail.com>
2024-12-17 10:30:27 +0530

    dfghjk

---
+++ script.js
@@ -0,0 +1,10 @@
+let yes=0;
+let no=0;
+function yesf(){
+    yes++;
+    document.getElementById("yes").innerHTML = "Yes:" +yes;
+}
+function nof(){
+    no++;
+    document.getElementById("no").innerHTML = "No:" +no;
+}
